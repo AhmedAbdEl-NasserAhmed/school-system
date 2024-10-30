27f483a18bb57f301039cba1b73885a0c4b1ee16
unknown <nasserahmed093@gmail.com>
2024-10-31 02:16:09 +0300

    adding charts to the admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,41 @@
+import AttendenaceChart from "@/components/AttendenaceChar";
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>Admin Page</div>;
+  return (
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/*Left */}
+
+      <div className="w-full lg:w-2/3  flex flex-col gap-8  ">
+        <div className="flex justify-between items-center gap-4 flex-wrap">
+          {/*User Cards */}
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/*Middle Charts */}
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            {/*Count Charts */}
+            <CountChart />
+          </div>
+          {/*Attendance Charts */}
+
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendenaceChart />
+          </div>
+        </div>
+
+        {/*Bottom Charts */}
+      </div>
+
+      {/*Right */}
+      <div className="w-full lg:w-1/3">R</div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
+    <div className="flex">
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 ">
         <Link href="/" className="flex items-center lg:justify-start gap-2">
           <Image src="/logo.png" alt="Logo" width={32} height={32} />
@@ -17,7 +17,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa]">
         <NavBar />
         {children}
       </div>
