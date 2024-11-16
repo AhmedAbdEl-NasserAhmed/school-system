9e9fa6f625182172b4b0604887f673cf34f5459e
unknown <nasserahmed093@gmail.com>
2024-11-16 19:21:27 +0200

    adding big calendar component to parent page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,18 +1,34 @@
 import Announcements from "@/components/Announcements ";
-import BigCalendar from "@/components/BigCalendar";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 
-const ParentPage = () => {
+const ParentPage = async () => {
+  const { userId } = await auth();
+
+  const students = await prisma.student.findMany({
+    where: {
+      parentId: userId!
+    }
+  });
+
   return (
-    <div className="flex flex-col flex-1 justify-between p-4 gap-4 xl:flex-row ">
-      {/* Left */}
-      <div className="w-full xl:w-2/3 bg-green-300 rounded-md ">
-        <div className="h-full bg-white rounded-md p-4 ">
-          <h2 className="font-semibold text-lg">Schedule (John Doe) </h2>
-          <BigCalendar />
-        </div>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="">
+        {students.map((student) => (
+          <div className="w-full xl:w-2/3" key={student.id}>
+            <div className="h-full bg-white p-4 rounded-md">
+              <h1 className="text-xl font-semibold">
+                Schedule ({student.name + " " + student.surname})
+              </h1>
+              <BigCalendarContainer type="classId" id={student.classId} />
+            </div>
+          </div>
+        ))}
       </div>
-      {/* Right */}
-      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <Announcements />
       </div>
     </div>
